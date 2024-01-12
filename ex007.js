c19c05d7842ef2e2e530aa252c83d58d4c674fe4
Erick <152448908+ErickGLBR@users.noreply.github.com>
2024-01-12 16:23:05 -0300

    new exjs

---
+++ ex007.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log(`A velocidade de seu carro e ${vel}Km/h`)
+if (vel > 60) { // Condicao simples
+    console.log(`Voce ultrapassou a velocidade permitia. MULTADO!`)
+}
+console.log(`Dirija sempre usando cindo de segurança!`)
