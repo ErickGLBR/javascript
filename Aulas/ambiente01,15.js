b9f96040f013c8c4118fae91dfaba3afe84ebce9
Erick <152448908+ErickGLBR@users.noreply.github.com>
2024-01-13 15:12:31 -0300

    s

---
+++ Aulas/ambiente01,15.js
@@ -0,0 +1,11 @@
+let num = [5, 8, 9, 3, 2]
+num.sort()
+num.push(1)
+console.log(num)
+console.log(`O vetor tem ${num.length} posicoes `)
+console.log(`O primeiro valor de vetor ${num[0]}`)
+console.log
+
+while (num != 0){
+    console.log("Qunatos numero tem ")
+}
