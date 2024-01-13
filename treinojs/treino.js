2d4c282eeb187e04e2d7acce7162648322d21bfa
Erick <152448908+ErickGLBR@users.noreply.github.com>
2024-01-13 14:44:11 -0300

    new

--- treinojs/treino.js
+++ treinojs/treino.js
@@ -1,6 +1,33 @@
-var pais = "Brasil"
-if (pais == "Brasil"){
-    console.log("Brasileiro")
-} else {
-    console.log("Estrangeiro")
+var c = 6
+do{
+    console.log(`Passo ${c}`)
+} while (c <= 6)
+
+
+
+/*
+do{
+    c <= 6 
+} while (c)
+*/
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++
 }
+*/
+
+
+
+
+/* 
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+
+*/
